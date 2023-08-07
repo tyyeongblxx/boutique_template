902d6d86ed8ad040c2b61a3ea344ff645427afd5
dmstjfgus <leetaeng2@naver.com>
2023-08-07 12:49:53 +0900

    best section...

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+// Button Hover Effect
+
+const btnObj = {
+  //DOM 컨트롤 클래스 요소 객체 정의
+  commonBtn: '.common-btn', //버튼 요소
+  wArrow: '.white', //흰색 화살표 요소
+  bArrow: '.black', // 검은색 화살표 요소
+};
+
+// 호버 시 화살표 색상 교체 함수 정의
+function btnHoverEffect(obj) {
+  console.log(obj);
+  const hoverBtn = document.querySelectorAll(obj.commonBtn);
+  console.log(hoverBtn);
+
+  hoverBtn.forEach((btn) => {
+    const wArrow = btn.document.querySelector(obj.wArrow);
+    const bArrow = btn.document.querySelector(obj.bArrow);
+
+    btn.addEventListener('mouseenter', function () {
+      wArrow.style.display = 'block';
+      bArrow.style.display = 'none';
+    });
+
+    btn.addEventListener('mouseleave', function () {
+      wArrow.style.display = 'none';
+      bArrow.style.display = 'block';
+    });
+  });
+}
+
+btnHoverEffect(btnObj); //함수 호출
