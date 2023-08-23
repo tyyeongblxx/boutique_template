e250f5316612859c8ff47565176b26f40db2e355
dmstjfgus <leetaeng2@naver.com>
2023-08-23 09:26:49 +0900

    1차 퍼브리싱 완료

--- js/custom.js
+++ js/custom.js
@@ -15,14 +15,14 @@ const swiperObj = {
 
 // 호버 시 화살표 색상 교체 함수 정의
 function btnHoverEffect(obj) {
-  console.log(obj);
+  // console.log(obj);
   const hoverBtn = document.querySelectorAll(obj.commonBtn);
-  console.log(hoverBtn);
+  // console.log(hoverBtn);
 
   hoverBtn.forEach((btn) => {
     const wArrow = btn.querySelector(obj.wArrow);
     const bArrow = btn.querySelector(obj.bArrow);
-    console.log(wArrow);
+    // console.log(wArrow);
 
     btn.addEventListener('mouseenter', function () {
       wArrow.style.display = 'block';
@@ -49,4 +49,37 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+});
+
+// mobile header toggle active
+// 1. 아이콘 클릭-> 2. 아이콘에 'on'클래스 추가(toggle) -> 3.네비게이션 높이저장 ->4.'on'클래스가 있을 때 내비게이션 활성화 -> 5.'on'클래스가 없을 때 내비게이션 비 활성화
+
+const menuIcon = document.querySelector('.menu-icon'); //메뉴아이콘 요소 저장
+const navi = document.querySelector('.navi'); //네비게이션 박스 요소 저장
+
+menuIcon.addEventListener('click', function () {
+  this.classList.toggle('on'); // menu-icon 클릭 할 때마다 on 클래스 추가 및 제거
+
+  const navHeight = navi.scrollHeight; //navi 박스의 자식요소를 포함한 높이
+  console.log(navHeight);
+
+  if (this.classList.contains('on')) {
+    // console.log(true); //menu icon에 on 클래스가있다면
+    navi.style.height = navHeight + 'px';
+  } else {
+    // console.log(false); //menu-icon에 on 클래스가 없다면
+    navi.style.height = 0;
+  }
+});
+
+//If PC size browser, navigation height to normal
+window.addEventListener('resize', function () {
+  const winWidth = window.outerWidth;
+
+  if (winWidth > 980) {
+    menuIcon.classList.remove('on');
+    navi.style.height = 'auto';
+  } else {
+    navi.style.height = 0;
+  }
 });
